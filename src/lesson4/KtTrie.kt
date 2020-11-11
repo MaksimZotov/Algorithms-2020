d0f797d3cd76ee23896127534c677bde455b8689
MaksimZotov <zotovmaksim1254@gmail.com>
2020-11-11 10:50:08 +0300

    comments and minor fixes

--- src/lesson4/KtTrie.kt
+++ src/lesson4/KtTrie.kt
@@ -1,9 +1,6 @@
 package lesson4
 
-import lesson2.calcPrimesNumber
-import java.lang.Exception
 import java.lang.StringBuilder
-import java.util.*
 
 /**
  * Префиксное дерево для строк
@@ -82,21 +79,70 @@ class KtTrie : AbstractMutableSet<String>(), MutableSet<String> {
         private var currentWord = mutableListOf<Char>()
         private var currentNode = root
 
+        // Время - если M > L
+        //      O(<= L * K + (L + L-1 + L-2 + L-3 + ... + 1) + (M - L)) = O(<= L * K + (1 + L)/2 * L + M - L) = O(<= L * K + L^2 + M - L)
+        //         иначе
+        //      O(M + 1)
+        //
+        // Память - O(1)
+        //
+        // M - длина пути - 1 от текущего слова до следющего, выраженная в количестве узлов
+        // L - длина ветки - 1 для текущего слова, выраженная в количестве узлов
+        // K - максимальная длина мапы из всех мап из узлов, составляющих путь от текущего слова до следующего
+        // Ki - длина мапы из i-го узла
+        //
+        // Пример:
+        //
+        //                 а
+        //           б       е
+        //      в          ж з и к л
+        //   г     д
+        //
+        // Если допустить, что имеются слова "абвд" и "аеж", то при переходе с текущего слова "абвд" на "аеж" вначале
+        //
+        // мы через findNode() перейдем из "абвд" в "абв" за 3 хода (L), где пройдём по 'г', 'д' (Ki = 2). Затем снова через
+        // findNode() из "абв" в "аб" - за 2 хода (L - 1) (тут проходим только по 'в'; Ki = 1),
+        // из "аб" в "а" - за 1 ход (L - 2).
+        //
+        // После этого перейдём из "а" в "ае" за 1 ход (тут проходим по 'б', 'е'; Ki = 2)
+        // и из "ае" в "аеж" тоже за 1 ход (тут проходим только по 'ж', хоть в "ае" есть и другие ключи; Ki = 1).
+        //
+        // Таким образом, в данном случае M = 5 (в - б - а - е - ж), K = 2, L = 3
+        //
+        // Другой пример:
+        //
+        //             а
+        //         б       в
+        //      г     д
+        //    е ж з
+        //
+        // При переходе из "абг" в "абд" сложность будет O(1)
+        //
+        // При переходе из "аб" в "абг" мы пройдемся только по 'г' (Ki = 1). Перейдём в "абг".
+        // Далее пройдемся только по 0.toChar() (Ki = 1), после чего получим "абг"
+        //
+        // Таким образом, M = 1 (г), K = 1, L = 0
+        //
+        // Важные замечания: если текущее и следующее слово лежат в одном узле, то сложность всегда O(0)
+        //
+        //                   если текущее слово находится в узле, являющимся родителем для следующего слова, то
+        //                   сложность всегда O((M + 1) * K) = O(M + 1), то есть L = 0, так как не будет вызовов findNode(), а
+        //                   Ki всегда равен 1.
+
         private fun getNext(lastChar: Char?): String {
             var lastChar = lastChar
             var lastCharIsFound = false
             while (true) {
                 var firstNotNull: Node? = null
                 var nextChar: Char? = null
+
+                // O(Ki)
                 for (char in currentNode.children.keys) {
-                    if (lastChar != null) {
-                        if (!lastCharIsFound && char != lastChar) {
-                            continue
-                        }
-                        if (!lastCharIsFound && char == lastChar) {
+                    if (lastChar != null && !lastCharIsFound) {
+                        if (char == lastChar) {
                             lastCharIsFound = true
-                            continue
                         }
+                        continue
                     }
                     if (char == 0.toChar()) {
                         word.clear()
@@ -121,14 +167,19 @@ class KtTrie : AbstractMutableSet<String>(), MutableSet<String> {
                     for (char in currentWord) {
                         wordForParentNode.append(char)
                     }
+
+                    // O(Li)
                     currentNode = findNode(wordForParentNode.toString())!!
                 }
             }
         }
 
+        // Время - O(1)
+        // Память - O(1)
         override fun hasNext(): Boolean =
             count != 0
 
+        // Сложность такая же, как у getNext()
         var nextWasInvoked = false
         override fun next(): String {
             var res = ""
@@ -150,6 +201,8 @@ class KtTrie : AbstractMutableSet<String>(), MutableSet<String> {
             return res
         }
 
+        // Время - O(1)
+        // Память - O(1)
         private var prevWordIsRemoved = false
         override fun remove() {
             if (currentWord.isEmpty() || prevWordIsRemoved) throw IllegalStateException()
